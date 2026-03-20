62d523fa32d2f3e731ba1564311d72f42c44a29f
Nahuel <nahuelpucciarelli@gmail.com>
2026-03-20 15:57:25 +0000

    arreglos, nuevo archivo codesys

--- Matlab/parametros.m
+++ Matlab/parametros.m
@@ -114,6 +114,7 @@ N_t = 8;                                            % Factor de separación para
 
 Kp_t = (2*zitta_t*wn_t*Mt)/Kf;                      % Ganancia proporcional lazo velocidad carro
 Ki_t = (wn_t^2*Mt)/Kf;                              % Ganancia integral lazo velocidad carro
+
 Kpos_t = wn_t/N_t;                                  % Ganancia lazo externo de posición carro
 Kvel_t = 0.75;                                      % Ganancia feedfoward velocidad lazo externo (orig 0.5)
 
@@ -133,6 +134,7 @@ N_h = 2.5;                                          % Factor de separación. Pue
 
 Kp_h = 2*zitta_h*wn_h*Jh_eq;                        % Ganancia proporcional lazo velocidad izaje
 Ki_h = (wn_h^2)*Jh_eq;                              % Ganancia integral lazo velocidad izaje
+
 Kpos_h = wn_h/N_h;                                  % Ganancia lazo externo de posición izaje
 Kvel_h = 0.5;                                       % Ganancia feedfoward velocidad lazo externo
 
